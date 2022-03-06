81042f71bbde945d4675d4c67d4cc70d0708f88e
kierstenv <kierstenvicknair22@gmail.com>
2022-03-06 17:58:57 -0600

    dynamic user repo fetch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-const getUserRepos = () => {
-  fetch("https://api.github.com/users/octocat/repos");
+const getUserRepos = (user) => {
+  const apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  fetch(apiUrl).then((response) => {
+    response.json().then((data) => {
+      console.log(data);
+    });
+  });
 };
 
-getUserRepos();
+getUserRepos("facebook");
+
+const response = fetch("https://api.github.com/users/octocat/repos");
